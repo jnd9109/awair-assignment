1d174fed9e18206a0a5d59ce209f6f28f2369506
Julien N <nguyenduy.j@gmail.com>
2021-09-15 15:35:46 +0900

    add user model

---
+++ awair/core/models.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timezone
+
+from django.db import models
+
+
+# Create your models here.
+class ParanoidModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True, db_index=True)
+    deleted_at = models.DateTimeField(db_index=True, null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+    def delete(self, using=None, keep_parents=False):
+        self.deleted_at = datetime.now(tz=timezone.utc)
+        self.save()
--- awair/custom_user/models.py
+++ awair/custom_user/models.py
@@ -1,3 +1,15 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from core.models import ParanoidModel
+
+
+class CustomUser(ParanoidModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=128, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False, unique=True)
+    password = models.CharField(max_length=128, blank=False, null=False)
+
+    def __str__(self):
+        return f'[{self.id}] {self.email}'
